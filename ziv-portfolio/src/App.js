6ff9f1397c68d5df78a8910d39867e02fb0dc4ce
ziv talmon <zivtalmon89@gmail.com>
2023-01-16 12:19:34 +0200

    adjustments

--- ziv-portfolio/src/App.js
+++ ziv-portfolio/src/App.js
@@ -10,7 +10,6 @@ import './styles/About.css'
 import './styles/Project.css'
 import './styles/Contact.css'
 
-// "homepage": "https://zifftalmon.github.io/zivPortfolio.github.io/",
 
 
 function App() {
