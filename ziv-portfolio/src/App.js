fab5ef0cd49a23539fe3168ef99adcca2f8b761b
ziv talmon <zivtalmon89@gmail.com>
2023-07-26 17:04:27 +0300

    first commit

--- ziv-portfolio/src/App.js
+++ ziv-portfolio/src/App.js
@@ -3,12 +3,12 @@ import About from "./components/About"
 import Contact from "./components/Contact"
 import Home from "./components/Home"
 import Project from "./components/Project"
+import './App.css';
 import './styles/Nav.css'
 import './styles/Home.css'
 import './styles/About.css'
 import './styles/Project.css'
 import './styles/Contact.css'
-import './App.css';
 
 
 
